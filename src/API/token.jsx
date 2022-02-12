a2592a3ae5a4ca9451d23a7dd9d51c383089ec90
Aleksandra Titova <61054408+alxtt@users.noreply.github.com>
2022-02-13 02:14:01 +0300

    deleted trash

--- src/API/token.jsx
+++
@@ -1,7 +0,0 @@
-import { useAuth0 } from "@auth0/auth0-react";
-
-async function getToken(){
-    const { getAccessTokenSilently } = useAuth0();
-    return await getAccessTokenSilently();
-
-}
--- src/AnimAllApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnimallappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AnimAllApp'
